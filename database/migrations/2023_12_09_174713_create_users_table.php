05300076107fbcbdb7ec6bd232e9341c1f054288
rawadabdallah <rawad0ne@gmail.com>
2023-12-09 19:53:36 +0200

    [core] JWT related commit

--- database/migrations/2023_12_09_174713_create_users_table.php
+++ database/migrations/2023_12_09_174713_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,12 +14,16 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('age');
             $table->rememberToken();
+            $table->unsignedBigInteger('id_user_type');
+            $table->foreign('id_user_type')->references('id_user_type')->on('user_types')->onDelete('cascade');
             $table->timestamps();
         });
     }
+
+
 
     /**
      * Reverse the migrations.
