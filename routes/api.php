f1b3930d90e6619bd95d173125afecee0fdcf6c9
rawadabdallah <rawad0ne@gmail.com>
2023-12-10 04:36:26 +0200

    [core] Setting Auth + Products controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+Route::controller(ProductsController::class)->group(function () {
+    Route::get('products', 'index');
+    Route::post('createProduct', 'createProduct');
+    Route::post('deleteProduct', 'deleteProduct');
+    Route::post('updateProduct', 'updateProduct');
 });
