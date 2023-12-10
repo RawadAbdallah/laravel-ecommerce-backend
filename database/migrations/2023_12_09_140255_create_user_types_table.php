1e96ff21e4337137aa284235dc671941f97d3a0f
rawadabdallah <rawad0ne@gmail.com>
2023-12-10 04:30:59 +0200

    [core] creating needed rows on creation

--- database/migrations/2023_12_09_140255_create_user_types_table.php
+++ database/migrations/2023_12_09_140255_create_user_types_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,6 +15,14 @@ return new class extends Migration
             $table->string('user_type', 16);
             $table->timestamps();
         });
+
+        DB::table('user_types')->insert(
+            [
+                ['user_type' => 'admin'],
+                ['user_type' => 'seller'],
+                ['user_type' => 'customer'],
+            ]
+        );
     }
 
     /**
